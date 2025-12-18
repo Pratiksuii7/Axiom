55c941cd5d4807f505ae44d911409c6eef0d42b2
Pratik <pratikadhikari904@gmail.com>
2025-12-18 16:37:32 +0545

    started to make a new project(wave simulator), completed html and declared some variables in js

---
+++ Wave Simulator/wave.js
@@ -0,0 +1,5 @@
+//lllets declare 1000 variables yayay
+var maincanv = document.getElementById('main-canvas');
+var mainctx = maincanv.getContext('2d');
+var heatcanv = document.getElementById('intensitycanvas');
+var heatctx = heatcanv.getContext('2d');
